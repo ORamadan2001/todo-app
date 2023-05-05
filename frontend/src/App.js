ff4c41f709c879299bf1458acaab77bc039275be
Omar Ramadan <oramadan2001@gmail.com>
2023-05-05 10:16:24 -0400

    Merge init

---
+++ frontend/src/App.js
@@ -0,0 +1,21 @@
+function App() {
+	return (
+		<div className="App">
+			<h1>Welcome!</h1>
+            <h4>To do list:</h4>
+            
+            <div classname = "todos">
+                <div classname = "todo">
+                    <div classname = "checkbox"> </div>
+
+                    <div classname = "task"> Learn how to use CSS! </div>
+
+                    <div classname = "delete">x</div>
+                </div>
+            </div>
+
+		</div>
+	);
+}
+
+export default App;
