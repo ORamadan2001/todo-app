3d30283bbbe239a602e0fedf3e0ab60cf7d3e48c
Omar Ramadan <oramadan2001@gmail.com>
2023-05-05 13:26:03 -0400

    'Completed' toggle and Delete functionality added.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,18 +1,74 @@
+import { useState, useEffect } from 'react';
+
+const API_BASE = "http://localhost:3001";
+
 function App() {
+    const[todos, setTodos] = useState([]);
+    const [popupActive, setPopupActive] = useState(false);
+    const [newTodo, setNewTodo] = useState("");
+
+    useEffect(() => {
+        GetTodos();
+    }, [])
+
+    const GetTodos = () => {
+        fetch(API_BASE + "/todos")
+        .then(res => res.json())
+        .then(data => setTodos(data))
+        .catch(err => console.error("Error: ", err));
+    }
+    
+    const completeTodo = async id => {
+        const res = await fetch(API_BASE + "/todo/complete/" + id, {
+          method: "PUT",
+          headers: {
+            "Content-Type": "application/json"
+          }
+        });
+        const data = await res.json();
+        
+        setTodos(todos => todos.map(todo => {
+          if (todo._id === data._id) {
+            todo.complete = data.complete;
+          }
+      
+          return todo;
+        }));
+      }
+      
+      const deleteTodo = async id => {
+        const data = await fetch(API_BASE + "/todo/delete/" + id, {
+            method: "DELETE"
+        }).then(res => res.json());
+
+        setTodos(todos => todos.filter(todo => todo._id !== data._id));
+
+      }
+
 	return (
 		<div className="App">
-			<h1>Welcome!</h1>
-            <h4>To do list:</h4>
+			<h1>Omar's To Do List</h1>
+            <h4>Tasks</h4>
             
-            <div classname = "todos">
-                <div classname = "todo">
-                    <div classname = "checkbox"> </div>
+            <div className = "todos">
+                {todos.map(todo => (
 
-                    <div classname = "task"> Learn how to use CSS! </div>
+                    <div className = {
+                        "todo " + (todo.complete ? "complete" : "")
+                    } key = {todo._id} onClick={() => completeTodo(todo._id)}>
+                        <div className = "checkbox"> </div>
 
-                    <div classname = "delete">x</div>
-                </div>
+                        <div className = "task"> {todo.text} </div>
+
+                        <div className = "delete" onClick={() => deleteTodo(todo._id)}>x</div>
+                    </div>
+
+                ))}
+        
+
             </div>
+
+            <div className = "addPopup" onClick={() => setPopupActive(true)}> + </div>
 
 		</div>
 	);
